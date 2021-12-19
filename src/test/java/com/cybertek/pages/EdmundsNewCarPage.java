37982f8a7f203b19d0fa1c8145da13c5f644993c
Murodil <murodil@cybertekschool.com>
2021-12-19 15:45:26 -0500

    DAY 23 - pages practice

--- src/test/java/com/cybertek/pages/EdmundsNewCarPage.java
+++ src/test/java/com/cybertek/pages/EdmundsNewCarPage.java
@@ -1,8 +1,20 @@
 package com.cybertek.pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.Select;
+
 import static org.testng.Assert.*;
 
 public class EdmundsNewCarPage extends BasePage {
+
+    @FindBy(name = "select-make")
+    public WebElement make;
+
+    public void selectMake(String carMake) {
+        Select makeDropDown = new Select(make);
+        makeDropDown.selectByVisibleText(carMake);
+    }
 
     @Override
     public void isCurrentPage() {
