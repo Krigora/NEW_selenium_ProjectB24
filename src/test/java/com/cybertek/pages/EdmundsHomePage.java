63841f35c2c51ec0748476ac3fd5426c30a28348
Murodil <murodil@cybertekschool.com>
2021-12-19 13:10:24 -0500

    DAY 23 - pages practice

--- src/test/java/com/cybertek/pages/EdmundsHomePage.java
+++ src/test/java/com/cybertek/pages/EdmundsHomePage.java
@@ -4,6 +4,14 @@ import static org.testng.Assert.*;
 
 public class EdmundsHomePage extends BasePage {
 
+    /**
+     * Automatically added by compiler.
+     *  no-args default constructor
+     *  super() call to parent class no-args constructor
+        public EdmundsHomePage() {
+            super();
+        }
+    */
     @Override
     public void isCurrentPage() {
         assertEquals(driver.getTitle(), "New Cars, Used Cars, Car Reviews and Pricing | Edmunds");
---
+++ src/test/java/com/cybertek/tests/day23_fluentwait_testNg_overview/EdmundsNewCarSearchTest.java
@@ -0,0 +1,17 @@
+package com.cybertek.tests.day23_fluentwait_testNg_overview;
+
+import com.cybertek.pages.EdmundsHomePage;
+import com.cybertek.tests.TestBase;
+import com.cybertek.utils.ConfigurationReader;
+import org.testng.annotations.Test;
+
+public class EdmundsNewCarSearchTest extends TestBase {
+    @Test
+    public void searchForNewCarTest() {
+        driver.get(ConfigurationReader.getProperty("edmunds.url"));
+        EdmundsHomePage homePage = new EdmundsHomePage();
+        homePage.isCurrentPage();
+        homePage.gotoNewCarPricingPage();
+
+    }
+}
