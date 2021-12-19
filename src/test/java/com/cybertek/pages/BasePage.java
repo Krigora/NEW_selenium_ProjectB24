b8079113a7cb7e0e393ad2979a7603010a8bd057
Murodil <murodil@cybertekschool.com>
2021-12-19 12:45:11 -0500

    DAY 23 - pages practice

--- src/test/java/com/cybertek/pages/BasePage.java
+++ src/test/java/com/cybertek/pages/BasePage.java
@@ -1,4 +1,31 @@
 package com.cybertek.pages;
 
+import com.cybertek.utils.Driver;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+/**
+ * this class is parent class for all page object classes
+ */
 public abstract class BasePage {
+    //Webdriver variable just in case any of children need
+    protected WebDriver driver;
+
+    @FindBy(xpath = "//img[@alt='Home']")
+    public WebElement edmundsLogo;
+
+    @FindBy(linkText = "New Car Pricing")
+    public WebElement newCarPricingLink;
+
+    public BasePage() {
+        driver = Driver.getDriver();
+        PageFactory.initElements(driver, this);
+    }
+
+    public void gotoNewCarPricingPage() {
+        newCarPricingLink.click();
+    }
+
 }
